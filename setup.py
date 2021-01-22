07a6b26f9360e7bc605b767489cc86c683b57fae
Spencer Lanman <slanman3755@gmail.com>
2021-01-22 03:15:38 -0800

    reduce python requirement to 3.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9, <4",
+    python_requires=">=3.7, <4",
     install_requires=[
         "click>=7.1.2",
         "neo4j-driver>=4.2.1",
